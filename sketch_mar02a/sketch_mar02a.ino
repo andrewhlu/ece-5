9cced6feb0ea94543339b6ae39339d567ac929d3
andrewhlu <alu@ucsb.edu>
2018-03-02 10:47:16 -0800

    Add two ultrasonics, add array functions

--- sketch_mar02a/sketch_mar02a.ino
+++ sketch_mar02a/sketch_mar02a.ino
@@ -4,18 +4,16 @@ Servo swiper;
 
 const int servoPin = 3;
 const int trigPin = 4;
-const int echoPin1 = 5;
-const int echoPin2 = 6;
+const int echoPin1 = 9;
+const int echoPin2 = 8;
 const int echoPin3 = 7;
-const int echoPin4 = 8;
-const int echoPin5 = 9;
+const int echoPin4 = 6;
+const int echoPin5 = 5;
 const int redPin = 10;
 const int greenPin = 11;
 const int bluePin = 12;
 
-int threshold = 150;
-
-//float distance1, distance2, distance3;
+const int threshold = 150;
 
 void setup() {
   swiper.attach(servoPin);
@@ -32,35 +30,41 @@ void setup() {
 }
 
 void loop() {
-  float distValues[5] = {getDistance(echoPin1), getDistance(echoPin2), getDistance(echoPin3), getDistance(echoPin4), getDistance(echoPin5)};
-  
-//  distance1 = getDistance(echoPin1);
-//  distance2 = getDistance(echoPin2);
-//  distance3 = getDistance(echoPin3);
+  float distValues[] = {getDistance(echoPin1), getDistance(echoPin2), getDistance(echoPin3), getDistance(echoPin4), getDistance(echoPin5)};
+  float minDist = minDistance(distValues);
 
   digitalWrite(redPin, LOW);
   digitalWrite(greenPin, LOW);
   digitalWrite(bluePin, LOW);
-  
-  if(distance1 < distance2 && distance1 < distance3 && distance1 < threshold) {
+
+  if(minDist == 0) {
     digitalWrite(redPin, HIGH);
+    swiper.write(0);
   }
-  else if(distance2 < distance1 && distance2 < distance3 && distance2 < threshold) {
-    digitalWrite(greenPin, HIGH);
-  }
-  else if(distance3 < distance1 && distance3 < distance2 && distance3 < threshold) {
+  else if(minDist == 1) {
+    digitalWrite(redPin, HIGH);
     digitalWrite(bluePin, HIGH);
+    swiper.write(45);
+  }
+  else if(minDist == 2) {
+    digitalWrite(greenPin, HIGH);
+    swiper.write(90);
+  }
+  else if(minDist == 3) {
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(bluePin, HIGH);
+    swiper.write(135);
+  }
+  else if(minDist == 4) {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, HIGH);
+    swiper.write(180);
   }
 
-  String outputString = String("L: ") + distance1 + " M: " + distance2 + " R: " + distance3;
+  String outputString = String("R: ") + distValues[0] + " Y: " + distValues[1] + " G: " + distValues[2] + " C: " + distValues[3] + " B: " + distValues[4];
 
   Serial.println(outputString);
   delay(50);
-
-  //swiper.write(180);
-  //delay(1000);
-  //swiper.write(0);
-  //delay(1000);
 }
 
 float getDistance(int readPin) {
@@ -77,4 +81,18 @@ float getDistance(int readPin) {
   }
   
   return pulse;
+}
+
+float minDistance(float *distArray) {
+  int lowestNum = 9999;
+  int lowestIndex = -1;
+
+  for(int i = 0; i < 5; i++) {
+    if(distArray[i] < lowestNum && distArray[i] < threshold) {
+      lowestNum = distArray[i];
+      lowestIndex = i;
+    }
+  }
+
+  return lowestIndex;
 }
